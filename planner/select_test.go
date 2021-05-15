08200df97ef9923be21696b958ffad25fc39933e
auxten <auxtenwpc@gmail.com>
2021-05-15 20:14:34 +0800

    Fix tests

--- planner/select_test.go
+++ planner/select_test.go
@@ -8,12 +8,14 @@ import (
 	"github.com/auxten/go-sqldb/db"
 	"github.com/auxten/go-sqldb/node"
 	"github.com/auxten/go-sqldb/parser"
+	"github.com/auxten/go-sqldb/utils"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestPlannerSelect(t *testing.T) {
 	Convey("Volcano model select implementation", t, func() {
 		const testFile = "test_plan_insert.db"
+		const InsertCnt = 512
 		defer func() {
 			_ = os.Remove(testFile)
 		}()
@@ -23,7 +25,7 @@ func TestPlannerSelect(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(table, ShouldNotBeNil)
 
-		for i := uint32(0); i < 32; i++ {
+		for i := uint32(0); i < InsertCnt; i++ {
 			row := &node.Row{
 				Id:       i,
 				Username: [32]byte{'a', 'u', 'x', 't', 'e', 'n', byte('a' + i)},
@@ -44,7 +46,9 @@ func TestPlannerSelect(t *testing.T) {
 		var i int
 		for row := range resultPipe {
 			i++
-			fmt.Println(row.Id, string(row.Username[:]), string(row.Email[:]))
+			fmt.Println(row.Id,
+				string(row.Username[:utils.Length(row.Username[:])]),
+				string(row.Email[:utils.Length(row.Email[:])]))
 		}
 		So(i, ShouldEqual, 1)
 
@@ -58,8 +62,10 @@ func TestPlannerSelect(t *testing.T) {
 		i = 0
 		for row := range resultPipe {
 			i++
-			fmt.Println(row.Id, string(row.Username[:]), string(row.Email[:]))
+			fmt.Println(row.Id,
+				string(row.Username[:utils.Length(row.Username[:])]),
+				string(row.Email[:utils.Length(row.Email[:])]))
 		}
-		So(i, ShouldEqual, 32)
+		So(i, ShouldEqual, InsertCnt)
 	})
 }
